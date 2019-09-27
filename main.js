33586f5500d3129161f5fb122e7bb86718116ecd
prasu93 <prasannamahesh93@gmail.com>
2019-09-27 13:35:04 -0500

    Edited to find the volume of cube

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const add = (x, y) => { return x + y }
+const vol = (x, y, z) => { return x * y * z }
 
 const validate = async (event) => {
   console.log(`triggered validate on ${event.target.id}`)
@@ -6,46 +6,39 @@ const validate = async (event) => {
   if (isValid) {
     event.target.nextElementSibling.innerHTML = ''
   } else {
-    event.target.nextElementSibling.innerHTML = 'Invalid input'
+    event.target.nextElementSibling.innerHTML = 'Invalid input(Enter value between 1 and 100)'
     event.target.focus()
   }
 }
 
-const updateWithAdd = async (event) => {
+const updateWithVolume = async (event) => {
   document.querySelector('#result').innerHTML = ''
-  if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
-    const regex = /[^a-zA-Z_]/g
-    const s = document.querySelector('#guest').value.replace(regex, '')
-    const i = parseInt(document.querySelector('#firstNumber').value)
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `${s}, your sum is ${add(i, j)}.`
+  if (document.querySelector('#len').checkValidity() && document.querySelector('#wid').checkValidity() && document.querySelector('#hei').checkValidity()) {
+    // const regex = /[^a-zA-Z_]/g
+    // const s = document.querySelector('#guest').value.replace(regex, '')
+    const l = parseInt(document.querySelector('#len').value)
+    const w = parseInt(document.querySelector('#wid').value)
+    const h = parseInt(document.querySelector('#hei').value)
+    const ans = `CUBE volume is ${vol(l, w, h)}.`
     document.querySelector('#result').innerHTML = ans
   }
 }
 
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
+
 
 // delegate to dynamic elements (e.g. when testing)
 // focusout is like blur, but it bubbles up
 
 document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
+  if ((event.target && event.target.id === 'len') ||
+    (event.target && event.target.id === 'wid') || (event.target && event.target.id === 'hei'))
+    {
     validate(event)
   }
 })
 
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
+  if (event.target && event.target.id === 'volButton') { updateWithVolume(event) }
 })
 
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-})
+
